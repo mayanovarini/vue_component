6fe9660ef9d0bdb7b9506da7cb77728c1302e769
thegirlfromipanema <mayanovarini@gmail.com>
2017-12-12 16:39:58 -0800

    create 2 components with their style scoped

---
+++ src/components/Module1.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Module 1</h1>
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+<style scoped>
+  h1 {
+  color: pink;
+  }
+</style>
---
+++ src/components/Module2.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Module 2</h1>
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+<style scoped>
+  h1 {
+  color: blue;
+  }
+</style>
